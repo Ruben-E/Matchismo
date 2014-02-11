9fcc8f2e95f729e3010695feefe8757329977499
Ruben Ernst <rubenernst@MacBook-Pro-van-Ruben.local>
2014-02-11 21:07:33 +0100

    Initial commit

---
+++ Matchismo/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Ruben Ernst on 11-02-14.
+//  Copyright (c) 2014 ruben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+
+}
