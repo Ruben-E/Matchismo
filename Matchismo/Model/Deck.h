ee4aaa7a76bb573abf156fb279f61441b301e1d6
Ruben Ernst <rubenernst@gmail.com>
2014-02-11 22:07:38 +0100

    Implemented Deck model. Renamed Desk to Deck (typo :) )

---
+++ Matchismo/Model/Deck.h
@@ -0,0 +1,15 @@
+//
+// Created by Ruben Ernst on 11-02-14.
+// Copyright (c) 2014 ruben. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Card.h"
+
+@interface Deck : NSObject
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop;
+- (void)addCard:(Card *)card;
+- (Card *)drawRandomCard;
+
+@end
