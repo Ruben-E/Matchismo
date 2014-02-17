955aed3089ce5f60cbfac75adfab64bcdf18a6a0
Ruben Ernst <rubenernst@gmail.com>
2014-02-17 17:38:05 +0100

    Created CardMatchingGame model

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,10 @@
+//
+// Created by Ruben Ernst on 17-02-14.
+// Copyright (c) 2014 ruben. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CardMatchingGame : NSObject
+@end
---
+++ Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,12 @@
+//
+// Created by Ruben Ernst on 17-02-14.
+// Copyright (c) 2014 ruben. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+
+@implementation CardMatchingGame {
+
+}
+@end
