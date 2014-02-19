60298adb031d5de0def960f40d48f3cfc46a58d4
Ruben Ernst <rubenernst@gmail.com>
2014-02-19 15:43:03 +0100

    Imlemented History object

---
+++ Matchismo/Model/History.h
@@ -0,0 +1,22 @@
+//
+// Created by Ruben Ernst on 18-02-14.
+// Copyright (c) 2014 ruben. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+enum Action{
+    toFront,
+    toBack,
+    matched,
+    notMatched
+} Action;
+
+@interface History : NSObject
+@property (nonatomic, strong) NSArray *cards;
+@property (nonatomic) NSInteger resultScore;
+@property (nonatomic) NSInteger totalScore;
+@property (nonatomic) NSUInteger totalFlips;
+@property (nonatomic, assign) enum Action action;
+
+@end
---
+++ Matchismo/Model/History.m
@@ -0,0 +1,12 @@
+//
+// Created by Ruben Ernst on 18-02-14.
+// Copyright (c) 2014 ruben. All rights reserved.
+//
+
+#import "History.h"
+
+
+@implementation History {
+
+}
+@end
