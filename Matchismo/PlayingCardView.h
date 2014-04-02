7dbfb6d77fe998475e2584708ee7a001f3d3da1f
Ruben Ernst <rubenernst@gmail.com>
2014-04-02 12:25:25 +0200

    Created PlayingCardView files

---
+++ Matchismo/PlayingCardView.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardView.h
+//  Matchismo
+//
+//  Created by Ruben Ernst on 02-04-14.
+//  Copyright (c) 2014 ruben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayingCardView : UIView
+
+@end
---
+++ Matchismo/PlayingCardView.m
@@ -0,0 +1,31 @@
+//
+//  PlayingCardView.m
+//  Matchismo
+//
+//  Created by Ruben Ernst on 02-04-14.
+//  Copyright (c) 2014 ruben. All rights reserved.
+//
+
+#import "PlayingCardView.h"
+
+@implementation PlayingCardView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
